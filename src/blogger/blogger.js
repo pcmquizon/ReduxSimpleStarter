086d1b16e494deb1140e946b5a67593879419e77
pcmquizon <camillemendoza17@gmail.com>
2017-09-11 18:21:37 +0800

    Blogger boilerplate

---
+++ src/blogger/blogger.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const Blogger = (props) => {
+  return(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <App />
+    </Provider>
+  );
+};
+
+export default Blogger;
--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -5,6 +5,7 @@ import StarterApp from '../starter/starter';
 import YoutubeApp from '../youtube/youtube';
 import LibraryApp from '../library/library';
 import WeatherForecastApp from '../weatherForecast/weatherForecast';
+import BloggerApp from '../blogger/blogger';
 import App from './components/app';
 
 const Dashboard = (props) => {
@@ -24,6 +25,10 @@ const Dashboard = (props) => {
     {
       name: 'Weather Forecast',
       content: <WeatherForecastApp />
+    },
+    {
+      name: 'Blogger',
+      content: <BloggerApp />
     },
 
   ];
